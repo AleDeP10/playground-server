1d182d06643abb6fdd76271847c6731d61559237
AleDeP10 <alessandro.deprato@outlook.com>
2024-12-01 10:36:54 +0100

    fix a risoluzione knexInstance in dbMiddleware

--- src/middleware/dbMiddleware.cjs
+++ src/middleware/dbMiddleware.cjs
@@ -1,16 +1,9 @@
-const knexInstance = require('../config/knexfile.cjs');
-
-// const addKnexToReq = (req, res, next) => {
-//   req.db = knexInstance;
-//   console.log('Knex instance added to request:', typeof req.db);
-//   next();
-// };
+const { knexInstance } = require('../config/knexfile.cjs');
 
 const addKnexToReq = (req, res, next) => {
-  req.db = knexInstance;
-  console.log('Knex instance added to request:', typeof req.db);  // Verifica che sia 'function'
+  req.db = knexInstance; // Assegna direttamente l'istanza knex
+  console.log('Knex instance added to request:', typeof req.db);  // Verifica che sia 'object'
   next();
 };
-
 
 module.exports = addKnexToReq;
