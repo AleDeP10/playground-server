3f7dae1108bf73c44c263d6bcbe8f9f917b91270
AleDeP10 <alessandro.deprato@outlook.com>
2024-12-03 09:24:15 +0100

    modificato controller hello

--- src/controllers/hello.cjs
+++ src/controllers/hello.cjs
@@ -1,11 +1,11 @@
 // @ts-nocheck
 
 const hello = async (req, res) => {
-  let name = 'World';
+  let name = 'Mondo';
   if (req.query.name) {
     name = req.query.name;
   }
-  const result = 'Hello, ' + name + '!';
+  const result = 'Ciao, ' + name + '!';
   res.status(200).json({ result });
   console.log('hello.controller', { name: req.query.name, result });
   return result;
